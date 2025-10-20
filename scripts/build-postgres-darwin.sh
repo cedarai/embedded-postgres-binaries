0ad451ffd4dc81a72eb4e448c7dc51e34d32a0a5
Aaron Evans <aaron@cedarai.com>
2025-10-20 15:12:27 -0400

    fix syntax

--- scripts/build-postgres-darwin.sh
+++ scripts/build-postgres-darwin.sh
@@ -29,7 +29,7 @@ if echo "$PG_VERSION" | grep -q '^9\.' && [ "$LITE_OPT" = true ] ; then
   echo "Lite option is supported only for PostgreSQL 10 or later!" && exit 1;
 fi
 
-export MACOSX_DEPLOYMENT_TARGET="$(sw_vers -productVersion)")
+export MACOSX_DEPLOYMENT_TARGET="$(sw_vers -productVersion)"
 
 ICU_ENABLED=$(echo "$PG_VERSION" | grep -qv '^9\.' && [ "$LITE_OPT" != true ] && echo true || echo false)
 
